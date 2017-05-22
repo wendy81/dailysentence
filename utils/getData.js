856cef1461a9abd7ff5a3d368cdc89fee9d9dc3c
wendy81 <457699029@qq.com>
2017-05-22 22:41:07 +0800

    update json

--- utils/getData.js
+++ utils/getData.js
@@ -2,6 +2,7 @@ function getData(params) {
  wx.request({
       url: 'https://raw.githubusercontent.com/wendy81/Learn-English-By-Picture/master/pages/data/data.json',
       success: res => {
+      	console.log(res.data);
         params.setData({images: res.data});
       }
   });
